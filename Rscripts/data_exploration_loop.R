0219e111873ea067ac31946d85ec94971071ad66
pastahl <123069008+pastahl@users.noreply.github.com>
2023-02-01 09:15:48 +0200

    Create data_exploration_loop.R

---
+++ Rscripts/data_exploration_loop.R
@@ -0,0 +1,24 @@
+
+#data exploration loop
+
+w_fw<-readRDS("../Data/weighted_foodwebs.rds")
+
+
+for(i in 1:length(w_fw)){
+  fw_i<-w_fw[[i]]
+  
+  fw<-graph_from_adjacency_matrix(
+    fw_i,
+    mode = c("directed"),
+    weighted = T
+  )
+  deg.sp<-degree(fw,mode = c("all"),loops = TRUE,normalized = FALSE)
+  sum.link.weights.sp<-apply(fw_i, 1, sum)+apply(fw_i, 2, sum)
+  names(sum.link.weights.sp)
+  names(deg.sp)
+  
+  year<-c("1981", "1986", "1991", "1996", "2001", "2006", "2011")
+  
+  plot(deg.sp[-c(2:4)], log(sum.link.weights.sp+1)[-c(2:4)], xlab="degree", ylab="sum.link.weights", main = year[i])
+
+}
