b92b35f60c302d5a5514fe277f37f0e2ded4941e
Susanne Kortsch <75016045+skortsch@users.noreply.github.com>
2024-12-16 13:59:42 +0200

    Delete Rscripts directory

--- Rscripts/load_data.R
+++
@@ -1,7 +0,0 @@
-
-setwd("C:/LocalData/susakort/GitHub/GoR-Food-webs/Rscripts")
-bm<-read.csv("../Data/biomass_GOR.csv", header=T)
-
-load("GoRoff_v2.Rdata")
-load("ConstSW5_GoRoff_v2.Rdata")
-load("ConstSW5_GoRoff_R1000.Rdata")
